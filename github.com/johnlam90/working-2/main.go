e1e9ae51c7cceedb80ae750a8481f841fe8903b2
johnlam90 <johnlam90@gmail.com>
2021-09-24 17:58:39 -0500

    first push for go

---
+++ github.com/johnlam90/working-2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// reqeusts to the path /goodbye with be handled by this function
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World!!,whats happening")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
+		//log.Printf("Data: %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
